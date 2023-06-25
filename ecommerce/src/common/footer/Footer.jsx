7afc1478c1b1c4c9ccb271703a3caca0a89ccbfb
MeshachArinze <meshacharinze@gmail.com>
2023-06-25 18:22:44 +0100

    commit to repo

--- ecommerce/src/common/footer/Footer.jsx
+++ ecommerce/src/common/footer/Footer.jsx
@@ -1,57 +1,72 @@
-import "./style.css"
+import "./style.css";
+import {
+  Footers,
+  FooterH1,
+  FooterH2,
+  FooterIcon,
+  FooterDiv,
+  FooterLI,
+  FooterPara,
+  FooterSpan,
+} from "./Footer.style";
 
 const Footer = () => {
   return (
     <>
-      <footer>
-        <div className='container grid2'>
-          <div className='box'>
-            <h1>Bonik</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat et lectus vel ut sollicitudin elit at amet.</p>
-            <div className='icon d_flex'>
-              <div className='img d_flex'>
-                <i className='fa-brands fa-google-play'></i>
-                <span>Google Play</span>
-              </div>
-              <div className='img d_flex'>
-                <i className='fa-brands fa-app-store-ios'></i>
-                <span>App Store</span>
+      <Footers>
+        <div className="container grid2">
+          <div className="box">
+            <FooterH1>Bonik</FooterH1>
+            <FooterPara>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor
+              libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat
+              et lectus vel ut sollicitudin elit at amet.
+            </FooterPara>
+
+            <div className="icon d_flex">
+              <FooterDiv className="img d_flex">
+                <FooterIcon className="fa-brands fa-google-play"></FooterIcon>
+                <FooterSpan>Google Play</FooterSpan>
+              </FooterDiv>
+              <div className="img d_flex">
+                <FooterIcon className="fa-brands fa-app-store-FooterIconos"></FooterIcon>
+                <FooterSpan>App Store</FooterSpan>
               </div>
             </div>
           </div>
 
-          <div className='box'>
-            <h2>About Us</h2>
+          <div className="box">
+            <FooterH2>About Us</FooterH2>
             <ul>
-              <li>Careers</li>
-              <li>Our Stores</li>
-              <li>Our Cares</li>
-              <li>Terms & Conditions</li>
-              <li>Privacy Policy</li>
+              <FooterLI>Careers</FooterLI>
+              <FooterLI>Our Stores</FooterLI>
+              <FooterLI>Our Cares</FooterLI>
+              <FooterLI>Terms & Conditions</FooterLI>
+              <FooterLI>Privacy PoFooterLIcy</FooterLI>
             </ul>
           </div>
-          <div className='box'>
-            <h2>Customer Care</h2>
+          <div className="box">
+            <FooterH2>Customer Care</FooterH2>
             <ul>
-              <li>Help Center </li>
-              <li>How to Buy </li>
-              <li>Track Your Order </li>
-              <li>Corporate & Bulk Purchasing </li>
-              <li>Returns & Refunds </li>
+              <FooterLI>Help Center </FooterLI>
+              <FooterLI>How to Buy </FooterLI>
+              <FooterLI>Track Your Order </FooterLI>
+              <FooterLI>Corporate & Bulk Purchasing </FooterLI>
+              <FooterLI>Returns & Refunds </FooterLI>
             </ul>
           </div>
-          <div className='box'>
-            <h2>Contact Us</h2>
+          <div className="box">
+            <FooterH2>Contact Us</FooterH2>
             <ul>
-              <li> ugbompro junction, beside corner stone</li>
-              <li>Email: meshacharinze@gmail.com</li>
-              <li>Phone: +1 1123 456 780</li>
+              <FooterLI> ugbompro junction, beside corner stone</FooterLI>
+              <FooterLI>Email: meshacharinze@gmail.com</FooterLI>
+              <FooterLI>Phone: 09037766269</FooterLI>
             </ul>
           </div>
         </div>
-      </footer>
+      </Footers>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
---
+++ ecommerce/src/common/footer/Footer.style.jsx
@@ -0,0 +1,46 @@
+import { styled } from "styled-components";
+
+export const Footers = styled.footer`
+  background-color: #0f3460;
+  padding: 100px 0;
+  color: #fff;
+`;
+
+export const FooterH1 = styled.h1`
+  margin-bottom: 30px;
+  color: #e94560;
+  font-weight: 800;
+  font-style: italic;
+`;
+
+export const FooterDiv = styled.div`
+  background-color: #0c2a4d;
+  padding: 15px 20px;
+  border-radius: 5px;
+`;
+
+export const FooterIcon = styled.i`
+  font-size: 20px;
+  margin: 5px;
+`;
+
+export const FooterSpan = styled.span`
+  margin-top: 5px;
+`; 
+
+export const FooterPara = styled.p`
+  font-size: 14px;
+  font-weight: 300;
+  opacity: 0.5;
+  margin-bottom: 20px;
+`;
+
+
+export const FooterH2 = styled.h2`
+  margin-bottom: 20px;
+`;
+
+export const FooterLI = styled.li`
+  margin-bottom: 10px;
+  opacity: 0.5;
+`;
